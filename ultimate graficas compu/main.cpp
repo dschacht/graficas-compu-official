c1d30d05f8c663042a204be95f551c695e40f8cb
Schacht <dashlun@hotmail.com>
2017-08-21 16:35:25 -0500

    version con open gl viejito

--- ultimate graficas compu/main.cpp
+++ ultimate graficas compu/main.cpp
@@ -1,13 +1,62 @@
 #include <iostream>
-using namespace std;
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 
-int main()
+void GameLoop()
 {
-	std::cout << "hola mundo!" << std::endl;
-	std::cin.get();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	return 0;
+	// ¡¡¡¡¡WARNING!!!!! Esto es OpenGL viejito. Sólamente lo vamos a usar esta clase.
+	// Prohibido el resto del semestre.
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-1.0f, -1.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2f(1.0f, -1.0f);
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex2f(0.0f, 1.0f);
+	glEnd();
+	// ¡¡¡¡¡END WARNING!!!!!
 
+	glutSwapBuffers();
 }
 
+int main(int argc, char* argv[])
+{
+	// Inicializar freeglut
+	// Freeglut se encarga de crear una ventana
+	// en donde podemos dibujar
+	glutInit(&argc, argv);
+	// Iniciar el contexto de OpenGL. El contexto se refiere
+	// a las capacidades que va a tener nuestra aplicación
+	// gráfica.
+	// En este caso estamos trabajando con el pipeline clásico.
+	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+	// Freeglut nos permite configurar eventos que ocurren en la ventana.
+	// Un evento que nos interesa es cuando alguien cierra la ventana.
+	// En este caso, simplemente dejamos de renderear la esscena y terminamos el programa.
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	// Configuramos el framebuffer. En este caso estamos solicitando un buffer
+	// true color RGBA, un buffer de profundidad y un segundo buffer para renderear.
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	// Iniciar las dimensiones de la ventana (en pixeles)
+	glutInitWindowSize(400, 400);
+	// Creamos la ventana y le damos un título.
+	glutCreateWindow("Hello World GL");
+	// Asociamos una función de render. Esta función se mandará a llamar para dibujar un frame.
+	glutDisplayFunc(GameLoop);
 
+	// Inicializar GLEW. Esta librería se encarga de obtener el API de OpenGL de
+	// nuestra tarjeta de video. SHAME ON YOU MICROSOFT.
+	glewInit();
+
+	// Configurar OpenGL. Este es el color por default del buffer de color
+	// en el framebuffer.
+	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+
+	// Iniciar la aplicación. El main se pausará en esta línea hasta que se cierre
+	// la venta de OpenGL.
+	glutMainLoop();
+
+	return 0;
+}
